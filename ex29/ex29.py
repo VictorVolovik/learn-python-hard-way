339c43790462c107b99c82dfea6bc28eb284837e
Victor Volovik <andhope2die@gmail.com>
2016-11-06 11:53:02 +0300

    ex29

---
+++ ex29/ex29.py
@@ -0,0 +1,28 @@
+""" copares earth species  """
+
+def compare_earth_species(people, cats, dogs):
+    """ compares people, cats and dogs """
+    if people < cats:
+        print("Too many cats! The world is doomed!")
+
+    if people > cats:
+        print("Not many cats! The world is saved!")
+
+    if people < dogs:
+        print("The world is drooled on!")
+
+    if people > dogs:
+        print("The world is dry!")
+
+    dogs += 5
+
+    if people >= dogs:
+        print("People are greater then or equal to dogs.")
+
+    if people <= dogs:
+        print("People are less or equal to dogs.")
+
+    if people == dogs:
+        print("People are dogs.")
+
+compare_earth_species(20, 30, 15)
