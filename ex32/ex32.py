cb0ef43e687e4ee64e21641767b0552d5f97dad6
Victor Volovik <andhope2die@gmail.com>
2016-11-07 08:42:21 +0300

    ex32

---
+++ ex32/ex32.py
@@ -0,0 +1,31 @@
+"""loops and lists"""
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print("This count %d" % number)
+
+# same as above
+for fruit in fruits:
+    print("A fruit of type: %s" % fruit)
+
+# also we cago through mixed lists too
+# notice we have to use %r since we don't know what's in it
+for i in change:
+    print("I got %r" % i)
+
+# we can also build lists, first start with an emty one
+numbers = []
+
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+    print("Adding %d to the list." % i)
+    # append adds item to a list
+    numbers.append(i)
+
+# now we can print them out too
+for number in numbers:
+    print("Element was: %d" % number)
