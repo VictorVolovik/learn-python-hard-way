ebeff369df0bbe1977210aa292abfeea26577701
Victor Volovik <andhope2die@gmail.com>
2016-11-13 18:46:37 +0300

    ex33

---
+++ ex33/ex33.py
@@ -0,0 +1,18 @@
+"""While-Loops"""
+
+i = 0
+numbers = []
+
+while i < 6:
+    print("At the top is %d" % i)
+    numbers.append(i)
+
+    i += 1
+
+    print("Numbers now: ", numbers)
+    print("At the bottom i is %d" % i)
+
+print("The numbers: ")
+
+for number in numbers:
+    print(number)
